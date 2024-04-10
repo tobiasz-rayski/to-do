3f09da4d665690d94c2a6d46c4416ead62039d46
tobiasz.rayski@gmail.com <124359533+tobiasz-rayski@users.noreply.github.com>
2024-04-10 18:19:35 +0200

    Init

---
+++ postcss.config.js
@@ -0,0 +1,5 @@
+// postcss.config.js
+const tailwindcss = require('tailwindcss')
+module.exports = {
+    plugins: ['postcss-preset-env', tailwindcss],
+}
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+  mode: "production",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    clean: true,
+  },
+  //devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    //new BundleAnalyzerPlugin(),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx", ".md"],
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+  },
+};
