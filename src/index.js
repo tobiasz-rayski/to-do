214aad74092b25471a8d579e242b0636cd52d58a
tobiasz-rayski <124359533+tobiasz-rayski@users.noreply.github.com>
2024-04-15 12:54:28 +0200

    feat: hamburger button using keyframes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "../src/styles/main.scss";
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", (e) => {
+  if (btn.classList.contains("open")) {
+    btn.classList.remove("open");
+    btn.classList.add("close");
+  } else if (btn.classList.contains("close")) {
+    btn.classList.remove("close");
+    btn.classList.add("open");
+  } else {
+    btn.classList.add("open");
+  }
+});
