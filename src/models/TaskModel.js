1a78776b3a55ca3c9a779e29b693ba1bad238c84
tobiasz.rayski@gmail.com <124359533+tobiasz-rayski@users.noreply.github.com>
2024-04-25 23:23:13 +0200

    feat: introduce a basic Task model

---
+++ src/models/TaskModel.js
@@ -0,0 +1,46 @@
+import { v4 as uuidv4 } from "uuid";
+
+export default class Task {
+  constructor(name, priority = 0) {
+    this.setId();
+    this.setName(name);
+    this.setPriority(priority);
+    this.isDone = false;
+  }
+
+  setId() {
+    if (!this.id) {
+      this.id = uuidv4();
+    } else {
+      console.error("ID has already been defined.");
+    }
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  setName(name) {
+    if (name.length < 1) {
+      throw new Error("Task name cannot be empty.");
+    }
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setPriority(priority) {
+    if (priority >= 0 && priority < 4) {
+      this.priority = priority;
+    } else {
+      console.error("Priority must be a number between 0 and 3.");
+      return;
+    }
+  }
+
+  toggleStatus() {
+    this.isDone = !this.isDone;
+  }
+}
